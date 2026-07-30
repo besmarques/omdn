0890033f84f256293978c0ef6aba3aefe35cab98
besmarques <besmarques@gmail.com>
2026-07-30 20:18:03 +0100

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 import process from 'node:process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 const port = Number(process.env.PORT ?? 3000);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.resolve(__dirname, '../dist');
 
 app.use(express.json());
 
@@ -13,6 +22,19 @@ app.get('/api', (req, res) => {
 	});
 });
 
+app.use('/api', (req, res) => {
+	res.status(404).json({
+		status: false,
+		message: 'API route not found',
+	});
+});
+
+app.use(express.static(distPath));
+
+app.get('/{*splat}', (req, res) => {
+	res.sendFile(path.join(distPath, 'index.html'));
+});
+
 app.listen(port, () => {
-	console.log(`OMDN API running at http://localhost:${port}`);
+	console.log(`OMDN running on port ${port}`);
 });
